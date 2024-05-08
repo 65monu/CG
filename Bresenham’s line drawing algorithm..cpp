06dd8774b60925d629516e78d7b112965799371d
Priyanshu kumar <119956971+65monu@users.noreply.github.com>
2024-05-09 01:57:47 +0530

    .

--- Bresenham’s line drawing algorithm..cpp
+++ Bresenham’s line drawing algorithm..cpp
@@ -1,61 +1,40 @@
-#include <cmath>
-#include <cstdlib>
+#include <stdio.h>
+#include <conio.h>
 #include <graphics.h>
-#include <iostream>
-using namespace std;
-void bresenhamLine(int x0, int y0, int x1, int y1, int val)
+void drawline(int x0, int y0, int x1, int y1)
 {
- if (x0 == x1 && y0 == y1)
- {
- putpixel(x1, y1, val);
- }
- else
- {
- int dx = x1 - x0;
- int dy = y1 - y0;
- float m = float(dy) / (float)(dx);
- if (m >= 1 || m <= 0)
- {
- cout << "ERROR: Slope must be between 0 and 1." << endl;
- exit(1);
- }
- int d = 2 * dy - dx;
- int del_E = 2 * dy;
- int del_NE = 2 * (dy - dx);
- int x = x0;
- int y = y0;
- putpixel(x, y, val);
- while (x < x1)
- {
- if (d <= 0)
- {
- d += del_E;
- x += 1;
- }
- else
- {
- d += del_NE;
- x += 1;
- y += 1;
- }
- putpixel(x, y, val);
- }
- }
- return;
+    int dx, dy, p, x, y;
+    dx = x1 - x0;
+    dy = y1 - y0;
+    x = x0;
+    y = y0;
+    p = 2 * dy - dx;
+    while (x < x1)
+    {
+        if (p >= 0)
+        {
+            putpixel(x, y, RED);
+            y = y + 1;
+            p = p + 2 * dy - 2 * dx;
+        }
+        else
+        {
+            putpixel(x, y, RED);
+            p = p + 2 * dy;
+        }
+        x = x + 1;
+    }
 }
-int main(void)
+int main()
 {
- int x0, y0, x1, y1;
- cout << "Enter Left Endpoint (x0 y0): ";
- cin >> x0 >> y0;
- cout << "Enter Right Endpoint (x1 y1): ";
- cin >> x1 >> y1;
- cout << "Drawing Line..." << endl;
- int gd = DETECT, gm;
- initgraph(&gd, &gm, NULL);
- bresenhamLine(x0, y0, x1, y1, WHITE);
- delay(5e3);
- closegraph();
- cout << "Finished..." << endl;
- return 0;
+    clrscr();
+    int gdriver = DETECT, gmode, error, x0, y0, x1, y1;
+    initgraph(&gdriver, &gmode, "c:\\turboc3\\bgi");
+    printf("Enter co-ordinates of first point: ");
+    scanf("%d%d", &x0, &y0);
+    printf("Enter co-ordinates of second point: ");
+    scanf("%d%d", &x1, &y1);
+    drawline(x0, y0, x1, y1);
+    getch();
+    return 0;
 }
